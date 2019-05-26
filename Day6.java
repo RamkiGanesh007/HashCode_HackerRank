e3b4a09bba516c18747dd338b909a17193401ee6
RamkiGanesh007 <33938427+RamkiGanesh007@users.noreply.github.com>
2019-05-26 15:41:04 +0530

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class P1 {
+
+    
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+       
+        for(;n>0;n--)
+        {
+        	 String Even="",Odd="";
+        	String Str=scanner.nextLine();
+        	int len=Str.length()-1;
+        	for(int j=0;j<=len;j++)
+        	{
+        		if(j%2==0)
+        		{
+        			Even=Even+Str.charAt(j);
+        		}
+        		else
+        		{
+        			Odd=Odd+Str.charAt(j);
+        		}
+        	}
+        	 System.out.println(Even+" "+Odd);
+        }
+       
+        scanner.close();
+}
+}
