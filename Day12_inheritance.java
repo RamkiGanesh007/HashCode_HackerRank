15a8580c561c06f0055343e303d329e544e6af2d
RamkiGanesh007 <33938427+RamkiGanesh007@users.noreply.github.com>
2019-06-12 08:44:39 +0530

    Create Day12_inheritance.java
    Error: Implicit super constructor is undefined for default constructor

---
+++ Day12_inheritance.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+class Person {
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+	 
+}
+
+class Student extends Person{
+	private int[] testScores;
+
+    Student(String firstName,String lastName,int id,int[] testScores)
+    {
+       
+        this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+        this.testScores=testScores; 
+    }
+
+    /*	
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    // Write your method here
+    char calculate()
+    {
+    	int avg=0;
+    	for(int i = 0; i < testScores.length; i++){
+			avg+=testScores[i];
+		}
+    	avg=avg/testScores.length;
+    	if(avg>=90&&avg<=100)
+    	{
+    		return 'O';
+    	}
+    	else if(avg>=80&&avg<=90)
+    	{
+    		return 'E';
+    	}
+    	else if(avg>=70&&avg<=80)
+    	{
+    		return 'A';
+    	}
+    	else if(avg>=55&&avg<=70)
+    	{
+    		return 'P';
+    	}
+    	else if(avg>=40&&avg<=55)
+    	{
+    		return 'D';
+    	}
+    	else if(avg<40)
+    	{
+    		return 'T';
+    	}
+    	return '0';
+    	}
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate());
+	}
+}
